d288de39f720334e7887f7e1b1d1e7640ffdbca2
Rupert Parry <rp@rupert.cloud>
2022-08-15 15:21:16 -0700

    Changed server URL

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,36 @@
-import { useState, useEffect } from 'react'
-import initialize from '../api/initialize'
-import updateParameter from '../api/updateParameter'
-import config from '../config.json'
+import { useState, useEffect } from 'react';
+import initialize from '../api/initialize';
+import updateParameter from '../api/updateParameter';
+import config from '../config.json';
 
-const baseUrl = process.env.NEXT_PUBLIC_ENV == "development" ? config.devBaseUrl : config.baseUrl
+const baseUrl = process.env.NEXT_PUBLIC_ENV == "development" ? config.devBaseUrl : config.baseUrl;
 
 
 function HomePage() {
-  const [sessID, setSessID] = useState('')
+  const [sessID, setSessID] = useState('');
   const [excitement, setExcitement] = useState(0.5);
 
   useEffect(() => {
     const getSessID = async () => {
-      const newID = await initialize()
-      setSessID(newID)
-    }
-    getSessID()
-  }, [])
+      const newID = await initialize();
+      setSessID(newID);
+    };
+    getSessID();
+  }, []);
 
   useEffect(() => {
-    if (!sessID) return
-    updateParameter(sessID, { excitement })
-  }, [excitement])
+    if (!sessID) return;
+    updateParameter(sessID, { excitement });
+  }, [excitement]);
 
   function handleChange(e) {
-    setExcitement(e.target.value / 100)
+    setExcitement(e.target.value / 100);
   }
 
   return (
     <div>
       <h1>Adapt Engine</h1>
-      <p><audio src={sessID ? `${baseUrl}/stream/${sessID}` : ''} controls></audio></p>
+      <p><audio src={sessID ? `${baseUrl}/${sessID}/stream.mp3` : ''} controls autoPlay></audio></p>
 
       <h2>Parameters</h2>
       <p>Excitement: {excitement}</p>
@@ -39,4 +39,4 @@ function HomePage() {
   );
 }
 
-export default HomePage
+export default HomePage;
